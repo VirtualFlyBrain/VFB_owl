65ff05dfb2eaa8d58aeae37eef9e16d93270757a
dosumis <dosumis@gmail.com>
2014-01-19 00:35:51 +0000

    Fixed cursor issues, db checking, expression.  Mapping to domains and clusters still broken.

--- src/code/mod/dict_cursor.py
+++ src/code/mod/dict_cursor.py
@@ -1,6 +1,36 @@
 #!/usr/bin/env jython
+import sys
+from com.ziclix.python.sql import zxJDBC # DB connection
 
 def dict_cursor(cursor):
-    description = [x[0] for x in cursor.description]
-    for row in cursor:
-        yield dict(zip(description, row))  # Not sure that yield is the best option here, as produces a 
+	description = [x[0] for x in cursor.description] # iterate over rows in cursor.description, pulling first element
+	l = []
+	for row in cursor: # iterate over rows in cursor
+		d = dict(zip(description, row))
+#	yield dict(zip(description, row))  # This yields an iterator.  Doesn't actually run until needed.
+		l.append(d)
+	return l
+
+        
+# def dict_cursor2(cursor):
+#     description = [x[0] for x in cursor.description]
+# 	dout = ()
+#     for row in cursor:
+#         #yield dict(zip(description, row))  # Not sure that yield is the best option here, as produces a 
+# 		#dout.append(dout[description]=row
+
+def get_cursor(user, pwd):
+	conn = zxJDBC.connect("jdbc:mysql://localhost/flycircuit", user, pwd, "org.gjt.mm.mysql.Driver") # For local testing
+	cursor =  conn.cursor()
+	return cursor
+
+def test(user, pwd):
+	conn = zxJDBC.connect("jdbc:mysql://localhost/flycircuit", user, pwd, "org.gjt.mm.mysql.Driver") # For local testing
+	cursor = conn.cursor()
+	cursor.execute("SELECT * FROM flycircuit_driver_map")
+	dc = dict_cursor(cursor)
+	for d in dc:
+		print d.items()
+
+	
+
