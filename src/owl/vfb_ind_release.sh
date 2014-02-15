9d0a4e427a33a43a0bbe4f2136d045e9a2246720
dosumis <dosumis@gmail.com>
2014-02-15 20:49:57 +0000

    release script

---
+++ src/owl/vfb_ind_release.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+export IND_FILES="--merge Cachero2010.owl --merge Ito2013.owl --merge Jenett2012.owl --merge Yu2013.owl --merge fb_features.owl --merge fc_ind.owl --merge flycircuit_direct_mappings.owl"
+export RELEASE="2014-01-29"
+
+# Merge ind files with specified release of fbbt
+echo ''
+echo "*** Merging ind files with $RELEASE version of fbbt ***"
+owltools http://purl.obolibrary.org/obo/fbbt/${RELEASE}/fbbt-non-classified.owl --merge fbbt_features.owl $IND_FILES -o fbbt_vfb_ind.owl
+
+# assert inferences and strip out redundancy  # This 
+echo ''
+echo "*** Asserting inferences and striping redundancy ***"
+owltools fbbt_vfb_ind.owl --reasoner elk --reasoner-ask-all --remove-indirect -a INDIVIDUALS -o fbbt_vfb_ind_pr_nr.owl # Some special magic here. Best ask the owltools devs if you want to know how it works.
