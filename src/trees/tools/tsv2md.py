b2557e6231f976473b3961d86966de23066ca5e6
dosumis <dosumis@gmail.com>
2014-05-13 21:31:41 +0100

    tsv to markdown table converter

---
+++ src/trees/tools/tsv2md.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import sys
+import re
+
+def tsv2md(line):
+    return "|" + re.sub("\t", "|", line) + "|"
+
+tsv_file = open(sys.argv[1], "r")
+
+tsv = []
+for line in tsv_file:
+    tsv.append(line.rstrip("\n"))
+
+col_num = len(tsv[0].split("\t"))
+
+print (tsv2md(tsv.pop(0)))
+print "|" + ("+|" * col_num)
+
+for line in tsv:
+    print tsv2md(line)
+
