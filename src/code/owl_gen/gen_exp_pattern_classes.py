d6b4728ee2b580262f564f1da7d1346dc0fa4e13
David Osumi-Sutherland <dosumis@gmail.com>
2014-08-19 11:57:45 +0100

    Update gen_exp_pattern_classes.py

--- src/code/owl_gen/gen_exp_pattern_classes.py
+++ src/code/owl_gen/gen_exp_pattern_classes.py
@@ -24,16 +24,16 @@ def expression_annotation_to_owl(cursor, ont_dict, FBrf):
 	
 	## Currently running by FBrf
 	cursor.execute("SELECT DISTINCT fbbt.cvterm_name AS anatomy, fbbt.db_name AS idp, "
-	"fbbt.accession, teg.transgene_name, teg.transgene_uniquename " \
-	"FROM vfbview_fbbt fbbt " \
-  "JOIN expression_cvterm ec1 ON (fbbt.cvterm_id = ec1.cvterm_id) " \
-  "JOIN expression_cvterm ec2 ON (ec2.expression_id = ec1.expression_id) " \
-  "JOIN cvterm stage ON (stage.cvterm_id = ec2.cvterm_id) " \
-  "JOIN feature_expression fe ON (ec1.expression_id = fe.expression_id) " \
-  "JOIN pub ON (fe.pub_id = pub.pub_id) " \
-  "JOIN vfbview_transgene_expressed_gp teg ON (teg.gp_feature_id = fe.feature_id) " \
-	"-- WHERE teg.transgene_name = '' " \
-	"AND pub.uniquename = '%s'" % FBrf) 
+"fbbt.accession, teg.transgene_name, teg.transgene_uniquename " \
+"FROM vfbview_fbbt fbbt " \
+"JOIN expression_cvterm ec1 ON (fbbt.cvterm_id = ec1.cvterm_id) " \
+"JOIN expression_cvterm ec2 ON (ec2.expression_id = ec1.expression_id) " \
+"JOIN cvterm stage ON (stage.cvterm_id = ec2.cvterm_id) " \
+"JOIN feature_expression fe ON (ec1.expression_id = fe.expression_id) " \
+"JOIN pub ON (fe.pub_id = pub.pub_id) " \
+"JOIN vfbview_transgene_expressed_gp teg ON (teg.gp_feature_id = fe.feature_id) " \
+"-- WHERE teg.transgene_name = '' " \
+"AND pub.uniquename = '%s'" % FBrf) 
 
   fb_feat = ont_dict['fb_feat']  # URI pattern: http://flybase.org/reports/FBtp0062283
 	fbbt = ont_dict['fbbt'] #
