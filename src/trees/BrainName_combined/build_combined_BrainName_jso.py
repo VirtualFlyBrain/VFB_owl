ac8e945e15604f7c6c9d157b52c808e691c0a2fc
David Osumi-Sutherland <dosumis@gmail.com>
2015-02-27 17:29:47 +0000

    adding combined tract & domain tree

---
+++ src/trees/BrainName_combined/build_combined_BrainName_jso.py
@@ -0,0 +1,40 @@
+from tsv2pdm import tab
+from json_tree_tools import blank_treeContent_node, write_json, add_leaf, load_json, roll_readable_tree, get_nodeId_name
+import operator
+
+#  Assume existing, valid domain tree files
+#  Add two new nodes - one for tracts and one for neuropils?  - But do we have a terms?  
+#  Definitely not for neuropils...  
+
+# Or could try interleaving node. 
+# TO do this, need a list of subnodes  # 
+
+# Or just bung them on the end...
+
+dts = load_json("../BrainName_domains/json/treeStructure.jso")
+dtc = load_json("../BrainName_domains/json/treeContent.jso")
+ttc_tab = tab("../BrainName_tracts/", "domain_data.tsv")
+
+# sort table on name field (ultimately) => alphabetic ordering of tree
+ttc_tab.tab.sort(key=operator.itemgetter('name'))
+
+D_nodeIds = []
+for n in dtc:
+    D_nodeIds.append(int(n['nodeId']))  
+i = max(D_nodeIds) 
+
+for r in ttc_tab.tab:
+    if r['oboId']:
+        r['domainID'] = str(int(r['domainID']) + 100)
+        n = blank_treeContent_node(domainId=r['domainID'], nodeId=str(i), name = r['name'], oboID = r['oboId'], 
+                           color = r['domainColour'], centre = r['domainCentre'])
+        dtc.append(n)
+        add_leaf(str(i), dts, '0')
+        i += 1
+        
+idn = get_nodeId_name(dtc)
+roll_readable_tree(idn, dts)
+
+write_json(json_var = dtc, path = "json/treeContent.jso")
+write_json(json_var = dts, path = "json/treeStructure.jso")
+#ttc_tab.save_tab()
