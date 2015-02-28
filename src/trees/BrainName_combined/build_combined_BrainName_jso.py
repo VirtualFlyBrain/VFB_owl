d514875e69e5de164a3e504dc97b39fc0fe47f45
David Osumi-Sutherland <dosumis@gmail.com>
2015-02-28 12:39:14 +0000

    new version of combined tree - removing duplicates. Plus script for generating it.  Also new domainIds in tract domain map.

--- src/trees/BrainName_combined/build_combined_BrainName_jso.py
+++ src/trees/BrainName_combined/build_combined_BrainName_jso.py
@@ -15,26 +15,36 @@ dts = load_json("../BrainName_domains/json/treeStructure.jso")
 dtc = load_json("../BrainName_domains/json/treeContent.jso")
 ttc_tab = tab("../BrainName_tracts/", "domain_data.tsv")
 
+dlist = []
+for d in dtc:
+    if 'extId' in d.keys():
+        if len(d['extId']) > 0:
+            dlist.append(d['extId'][0])
+
 # sort table on name field (ultimately) => alphabetic ordering of tree
 ttc_tab.tab.sort(key=operator.itemgetter('name'))
 
 D_nodeIds = []
 for n in dtc:
     D_nodeIds.append(int(n['nodeId']))  
-i = max(D_nodeIds) 
+i = max(D_nodeIds) + 1
+
 
 for r in ttc_tab.tab:
+    r['domainID'] = str(int(r['domainID']) + 300) # Bump up domain id before adding
+    # if domain is mapped
     if r['oboId']:
-        r['domainID'] = str(int(r['domainID']) + 100)
-        n = blank_treeContent_node(domainId=r['domainID'], nodeId=str(i), name = r['name'], oboID = r['oboId'], 
-                           color = r['domainColour'], centre = r['domainCentre'])
-        dtc.append(n)
-        add_leaf(str(i), dts, '0')
-        i += 1
+        # if not already in tree - append to end
+        if r['oboId'] not in dlist:
+            n = blank_treeContent_node(domainId=r['domainID'], nodeId=str(i), name = r['name'], oboID = r['oboId'], 
+                                       color = r['domainColour'], centre = r['domainCentre'])
+            dtc.append(n)
+            add_leaf(str(i), dts, '0')
+            i += 1
         
 idn = get_nodeId_name(dtc)
 roll_readable_tree(idn, dts)
 
 write_json(json_var = dtc, path = "json/treeContent.jso")
 write_json(json_var = dts, path = "json/treeStructure.jso")
-#ttc_tab.save_tab()
+ttc_tab.save_tab()
