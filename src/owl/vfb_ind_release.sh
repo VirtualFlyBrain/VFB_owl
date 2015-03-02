95f9f0d68873d454131bfb51f8883735b4fa5793
David Osumi-Sutherland <dosumis@gmail.com>
2015-03-02 11:05:03 +0000

    Edits to ensure failure to build intermediate files cases  whole build to fail.

--- src/owl/vfb_ind_release.sh
+++ src/owl/vfb_ind_release.sh
@@ -6,15 +6,22 @@ DSSTRING=''
 
 cd ../code/owl_gen
 
+STAT=0
+ERROR_SUMMARY=""
 for var in ${DATASETS[@]}
 # java -classpath lib/*.jar:. my.package.Program
 do
     echo $var
-    java -cp $CP"*" org.python.util.jython vfb_ind_runner.py $USR $PD $var $FBBT
+    java -Xmx6000m -cp $CP"*" org.python.util.jython vfb_ind_runner.py $USR $PD $var $FBBT
     DSSTRING+="--merge ${var}.owl "
+    STAT+=$?
+    echo $STAT
+    ERROR_SUMMARY=${ERROR_SUMMARY}${var}" build failed\n"
 done
 
-java -cp $CP"*" org.python.util.jython fc_ind.py $USR $PD $FBBT
+java -Xmx6000m -cp $CP"*" org.python.util.jython fc_ind.py $USR $PD $FBBT
+STAT+=$?
+ERROR_SUMMARY=${ERROR_SUMMARY}${var}" build failed\n"
 
 DSSTRING+="--merge flycircuit_plus.owl --merge flycircuit_direct_mappings.owl "
 
@@ -30,3 +37,7 @@ owltools $FBBT --merge fb_features.owl --merge vfb_ext.owl $DSSTRING -o file://`
 echo ''
 echo "*** Asserting inferences and striping redundancy ***"
 owltools fbbt_vfb_ind.owl --reasoner elk --reasoner-ask-all --remove-indirect -a INDIVIDUALS -o -f ofn fbbt_vfb_ind_pr_nr.owl # Some special magic here. Best ask the owltools devs if you want to know how it works.
+
+echo $ERROR_SUMMARY
+return $STAT
+
