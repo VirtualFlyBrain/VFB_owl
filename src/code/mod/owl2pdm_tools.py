188fb279f2b9522fe5c4be8a553600163482aa9b
David Osumi-Sutherland <dosumis@gmail.com>
2015-05-20 16:24:18 +0100

    refactored around general ont manager class

--- src/code/mod/owl2pdm_tools.py
+++ src/code/mod/owl2pdm_tools.py
@@ -6,6 +6,60 @@ from java.util import TreeSet
 
 import warnings
 
+
+class simpleClassExpression():
+    """Takes an OWL API classExpression as arg.
+    Checks whether, if anonymous, it consists of
+     just one op and one class.
+     """     
+     
+    def __init__(self, classExpression):
+        self.ce = classExpression
+        self.classes = list(self.ce.getClassesInSignature())
+        self.rels = list(self.ce.getObjectPropertiesInSignature())
+        self.is_simple()
+        
+    def is_simple(self):
+        """Tests whether, if anonymous, there is only one object property and one class."""
+        #Ideally would also check quantifier
+        if self.ce.isAnonymous():
+            if len(self.rels) > 1:
+                warnings.warn("Class expression, %s, too complex to process" % self.ce)
+                return False
+                raise
+        if len(self.classes) > 1:
+            warnings.warn("Class expression, %s, too complex to process" % self.ce)
+            return False
+            raise
+        else:
+            return True  
+    
+    def get_class_sfid(self):
+        """Returns a shortFormId for the class in the simple class expression"""
+        ssfp = SimpleShortFormProvider()
+        return ssfp.getShortForm(self.classes[0])
+        
+    def get_rel_sfid(self):
+        """Returns a shortFormId for the relation (OP) in the simple class expression"""
+        ssfp = SimpleShortFormProvider()
+        return ssfp.getShortForm(self.rels[0])
+    
+    def get_sce_pdm(self):
+        od = {}
+        if self.ce.isAnonymous():
+            od['isAnonymous'] = True
+            od['relId'] = self.get_rel_sfid()
+        else:
+            od['isAnonymous'] = False
+        od['objectId'] = self.get_class_sfid()
+        return od
+    
+    def get_sce_as_manchester(self):
+        if not self.ce.isAnonymous():
+            return self.get_class_sfid()
+        else:
+            return "%s some %s" % (self.get_rel_sfid(), self.get_class_sfid())
+
 class ont_manager():
     # I seem to be reinventing Brain....
     """Constructor: ont_manager(OWLontology)
@@ -27,18 +81,26 @@ class ont_manager():
         #public BidirectionalShortFormProviderAdapter(OWLOntologyManager man,
         #java.util.Set<OWLOntology> ontologies,
         #ShortFormProvider shortFormProvider) # Providing the manager, means that this listens for changes.
-        self.bi_sfp = BidirectionalShortFormProviderAdapter(self.manager, ontset, self.simple_sfp) # .getShortForm(iri); .getEntity(
+        self.bi_sfp = BidirectionalShortFormProviderAdapter(self.manager, ontset, self.simple_sfp) # .getShortForm(iri); .getEntity()
+    
+    def get_ind_from_iri(self, iri_string):
+        iri = IRI.create(iri_string)
+        return self.factory.getOWLNamedIndividual(iri)
         
-
-    def get_types_for_ind(self, iri_string):
+    def get_types_for_ind(self, iri_string = '', sfid = ''):
         """iri_string: An iri string referencing an individual
         ont: An owlAPI ontology object for an ontology that 
         includes the referenced individual.
         Returns an iterable set of class expressions
         """
-        # Could refactor to generic get owl entity fn.       
-        iri = IRI.create(iri_string)
-        i = self.factory.getOWLNamedIndividual(iri)
+        # Could refactor to generic get owl entity fn.
+        i = ''
+        if sfid:
+            i = self.bi_sfp.getEntity(sfid)
+        elif  iri_string:
+            i = self.get_ind_from_iri(iri_string)
+        else:
+            warnings.warn("Method requires either iri string or shortFormID to be specified")
         return i.getTypes(self.ont) # 
     
     def get_axioms_ref_class(self, iri_string):
@@ -47,19 +109,56 @@ class ont_manager():
         c = self.factory.getOWLClass(iri)
         return c.getReferencingAxioms(self.ont)#? 
 
-    def get_sc_axioms_on_class(self, iri_string):
-        """Returns all superclasses of class."""
-        iri = IRI.create(iri_string)    
-        c = self.factory.getOWLClass(iri)
+    def get_sc_axioms_on_class(self, iri_string = '', sfid =''):
+        """Returns an interable set of asserted superclasses of class."""
+        c = ''
+        if sfid:
+            c = self.bi_sfp.getEntity(sfid)
+        else:
+            iri = IRI.create(iri_string)    
+            c = self.factory.getOWLClass(iri)
         return c.getSuperClasses(self.ont)#?
     
     def get_version_iri(self):
+        """Wot is says on' tin.  Returns string"""
         oid = self.ont.get_id()
         version_iri = oid.getVersionIRI()
-        return version_iri.toString()
+        return version_iri.toString()    
+    
+    def typeAxioms2pdm(self, iri_string = '', sfid = ''):
+    
+        """iri_string: An iri string referencing an individual
+        ont: An owlAPI ontology object for an ontology that includes the referenced individual
+        Returns a python datamodel consisting of a list of dicts with keys: 
+        isAnonymous:boolean; 
+        relId:URI_string; 
+        objectId:URI_string.
+        """
+        
+        types = self.get_types_for_ind(iri_string = iri_string, sfid = sfid)    
+        out = [] # Output array
+        for t in types:
+            sce = simpleClassExpression(t)
+            if sce.is_simple():
+                out.append(sce.get_sce_pdm())
+        return out
+    
+def migrate_axioms_to_ind(brain, claz):
+    """Finds all instances of a specified class, and migrates asserted
+    subclassing axioms down from the specified class to the individuals found."""
+    exp_pat_inds = brain.getInstances(claz, 0)
+    onto = brain.getOntology()
+    om = ont_manager(onto)
+    # get an iterable set of axioms
+    sc_ax = om.get_sc_axioms_on_class(sfid = claz)
+    for i in exp_pat_inds:
+        for ax in sc_ax:
+            sce = simpleClassExpression(ax)
+            # Should really do this with a native owl-api method...
+            brain.type(sce.get_sce_as_manchester(), i)
     
 
-# Keeping this as a separate free standing function for now, until dependent scripts refactored
+# Keeping these as a separate free standing function for now, until dependent scripts refactored
 def get_types_for_ind(iri_string, ont):
     """iri_string: An iri string referencing an individual
     ont: An owlAPI ontology object for an ontology that 
@@ -73,41 +172,6 @@ def get_types_for_ind(iri_string, ont):
     factory = manager.getOWLDataFactory()
     i = factory.getOWLNamedIndividual(iri)
     return i.getTypes(ont) # 
-
-
-
-class simpleClassExpression():
-    """Takes an OWL API classExpression as arg.
-    Checks whether, if anonymous, it consists of
-     just one op and one class.
-     """     
-     
-    def __init__(self, classExpression):
-        self.ce = classExpression
-        self.classes = list(self.ce.getClassesInSignature())
-        self.rels = list(self.ce.getObjectPropertiesInSignature())
-        self._test_simplicity()
-        
-    def _test_simplicity(self):
-        """Tests whether, if anonymous, there is only one object property and one class."""
-        #Ideally would also check quantifier
-        if not self.ce.isAnonymous():
-            if len(self.rels) > 1:
-                warnings.warn("Class expression, %s, too complex to process" % self.ce)
-            if len(self.classes) > 1:
-                warnings.warn("Class expression, %s, too complex to process" % self.ce)
-    
-    def get_class_sfid(self):
-        """Returns a shortFormId for the class in the simple class expression"""
-        ssfp = SimpleShortFormProvider()
-        return ssfp.getShortForm(self.classes[0])
-        
-    def get_rel_sfid(self):
-        """Returns a shortFormId for the relation (OP) in the simple class expression"""
-        ssfp = SimpleShortFormProvider()
-        return ssfp.getShortForm(self.rels[0])
-        
-    
 
 
 def typeAxioms2pdm(iri_string, ont):
