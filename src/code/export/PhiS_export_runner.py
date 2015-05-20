64a85170ec5502f52d73c827380b6b99cd1bfae2
David Osumi-Sutherland <dosumis@gmail.com>
2015-05-20 16:20:46 +0100

    now works for export to PhIS

--- src/code/export/PhiS_export_runner.py
+++ src/code/export/PhiS_export_runner.py
@@ -1,9 +1,21 @@
 from uk.ac.ebi.brain.core import Brain
-from owl2pdm_tools import typeAxioms2pdm
+from owl2pdm_tools import typeAxioms2pdm, ont_manager
 import  json
 
-# A rather Perlish datastructure.  The result of writing to JSON as an intermediate.
-# TODO: add field to record general classification - either via extension to Brain, or a cruder Python look
+
+ont_url = ''
+outfile = ''
+
+def gen_pdm(ont, inds, classification):   
+    axioms = {}
+    om = ont_manager(ont.getOntology())  
+    for i in inds:
+        axioms[i] = {}
+        axioms[i]["Types"] = om.typeAxioms2pdm(sfid = i)
+        axioms[i]["label"] = ont.getLabel(i)
+        axioms[i]["def"] =  ont.getAnnotation(i, "IAO_0000115")
+        axioms[i]["general_classification"] = classification            
+    return axioms
 
 
 def gen_pdm_from_indfile(ont_url, outfile):
@@ -21,24 +33,23 @@ def gen_pdm_from_indfile(ont_url, outfile):
     
     ont = Brain()
     ont.learn(ont_url)
-    onto = ont.getOntology()    
     axioms = {}
-
-    
-    def gen_pdm(ont, inds, classification):   
-        axioms = {} 
-        for i in inds:
-            axioms[i] = {}
-            axioms[i]["Types"] = typeAxioms2pdm("http://www.virtualflybrain.org/owl/" + i, onto) # Hard wired baseURI - should pass!
-            axioms[i]["label"] = ont.getLabel(i)
-            axioms[i]["def"] =  ont.getAnnotation(i, "IAO_0000115")
-            axioms[i]["general_classification"] = classification            
-        return axioms
-   
-    axioms.update(gen_pdm(ont.getInstances("B8C6934B-C27C-4528-BE59-E75F5B9F61B6", 0), "B8C6934B-C27C-4528-BE59-E75F5B9F61B6")) # expression_patterns
-    axioms.update(gen_pdm(ont.getInstances("FBbt_00005106", 0), "FBbt_00005106")) # neurons
-    axioms.update(gen_pdm(ont.getInstances("FBbt_00007683", 0), "FBbt_00007683")) # clones
+    if ont.knowsClass("B8C6934B-C27C-4528-BE59-E75F5B9F61B6"):
+        axioms.update(gen_pdm(ont, ont.getInstances("B8C6934B-C27C-4528-BE59-E75F5B9F61B6", 0), "B8C6934B-C27C-4528-BE59-E75F5B9F61B6")) # expression_patterns
+    if ont.knowsClass("FBbt_00005106"):
+        axioms.update(gen_pdm(ont, ont.getInstances("FBbt_00005106", 0), "FBbt_00005106")) # neurons
+    if ont.knowsClass("FBbt_00007683"):
+        axioms.update(gen_pdm(ont, ont.getInstances("FBbt_00007683", 0), "FBbt_00007683")) # clones
     jfile = open(outfile, "w")
     jfile.write(json.dumps(axioms, sort_keys=True, indent=4))
     ont.sleep()
         
+
+def __main__():
+    datasets = ['Cachero2010', 'Ito2013', 'Yu2013', 'Jenett2012_full', 'flycircuit_plus']
+    #base = 'http://purl.obolibrary.org/obo/fbbt/vfb/'
+    base = 'file:///repos/VFB_owl/src/owl/'
+    for d in datasets:
+        gen_pdm_from_indfile(ont_url = base + d + ".owl", outfile = d + ".json")
+        
+__main__()
