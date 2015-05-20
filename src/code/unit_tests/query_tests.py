36357617a821274fd0291ed1940321824c6fe16e
David Osumi-Sutherland <dosumis@gmail.com>
2015-05-20 16:28:59 +0100

    refactored.  Now uses general testing class, configured from queries in a tsv file.

--- src/code/unit_tests/query_tests.py
+++ src/code/unit_tests/query_tests.py
@@ -2,6 +2,7 @@
 
 from uk.ac.ebi.brain.error import BrainException
 from uk.ac.ebi.brain.core import Brain
+from tsv2pdm import tab
 import warnings
 
 vfb_ind = Brain()
@@ -10,7 +11,7 @@ vfb_ind.learn("../../owl/fbbt_vfb_ind_pr_nr.owl")
 # Draft class for object-based unit test system.  This allows more info to be stored about queries and tests. Objects should be populated from a tsv.  Good potential for generating documentation straight from tsv or by using these objects...
 
 class Query:
-    """Class for testing queries against ontology + individuals files for VFB  #  Seems this is not right for multi-line doc....
+    """Class for testing queries against ontology + individuals files for VFB
     Attributes:
         query - DL query with shortFormIDs
         query_by_label - DL query with labels (for reference)
@@ -55,48 +56,48 @@ class Query:
 
 # Unit tests using dict.
 
-DL_QueriesByID = {}
-
-DL_QueriesByID['FBbt_00005106 that RO_0002131 some FBbt_00003624'] = "neuron that overlaps some 'adult brain'"
-DL_QueriesByID['FBbt_00005106 that RO_0002113 some FBbt_00003624'] = "neuron that has_presynaptic_terminal_in some 'adult brain'"
-DL_QueriesByID['FBbt_00005106 that RO_0002130 some FBbt_00003624'] = "neuron that has_postsynaptic_terminal_in some 'adult brain'"
-DL_QueriesByID['FBbt_00005106 that RO_0002110 some FBbt_00003624'] = "neuron that has_synaptic_terminal_in some 'adult brain'"
-DL_QueriesByID['FBbt_00005099 that RO_0002134 some FBbt_00003624'] = "neuron projection bundle' that innervates some 'adult brain'"
-DL_QueriesByID['FBbt_00007683 that RO_0002131 some FBbt_00003624'] = "'neuroblast lineage clone' that overlaps some 'adult brain'"
-DL_QueriesByID['FBbt_00005106 that RO_0002101 some FBbt_00005099'] = "neuron that fasciculates_with some 'neuron projection bundle'"
-DL_QueriesByID['FBbt_00005106 that BFO_0000050 some FBbt_00007683'] = "neuron that part_of some 'neuroblast lineage clone'"
-
-
-for Q, qtext in DL_QueriesByID.iteritems():
-    instances = vfb_ind.getInstances(Q, 0)
-    if instances:
-        print qtext + " - Individuals: " + str( len(instances) )
-    else:
-        warnings.warn(qtext + " has no instances!")
-    instances = vfb_ind.subClasses(Q, 0)
-    if subclasses:
-        print qtext + " - subClasses: " + str( len(subclasses) )
-    else:
-        warnings.warn(qtext + " has no subclasses!")
-
-        # Queries for individuals currently work on labels.  Unusually, the
-
-# roll name_id lookup
-
-name_id = {}
-I = vfb_ind.getInstances("Thing", 0)
-S = vfb_ind.SubClassOf("Thing", 0)
-
-while i in I:
-    name_id[vfb_ind.getLabel(i)] = i
-    
-while s in S:
-    name_id[vfb_ind.getLabel(s)] = s
-
-cluster = name_id['cluster']
-neuron = name_id['neuron']
-
-    # But how to get a list of object properties?
+# DL_QueriesByID = {}
+# 
+# DL_QueriesByID['FBbt_00005106 that RO_0002131 some FBbt_00003624'] = "neuron that overlaps some 'adult brain'"
+# DL_QueriesByID['FBbt_00005106 that RO_0002113 some FBbt_00003624'] = "neuron that has_presynaptic_terminal_in some 'adult brain'"
+# DL_QueriesByID['FBbt_00005106 that RO_0002130 some FBbt_00003624'] = "neuron that has_postsynaptic_terminal_in some 'adult brain'"
+# DL_QueriesByID['FBbt_00005106 that RO_0002110 some FBbt_00003624'] = "neuron that has_synaptic_terminal_in some 'adult brain'"
+# DL_QueriesByID['FBbt_00005099 that RO_0002134 some FBbt_00003624'] = "neuron projection bundle' that innervates some 'adult brain'"
+# DL_QueriesByID['FBbt_00007683 that RO_0002131 some FBbt_00003624'] = "'neuroblast lineage clone' that overlaps some 'adult brain'"
+# DL_QueriesByID['FBbt_00005106 that RO_0002101 some FBbt_00005099'] = "neuron that fasciculates_with some 'neuron projection bundle'"
+# DL_QueriesByID['FBbt_00005106 that BFO_0000050 some FBbt_00007683'] = "neuron that part_of some 'neuroblast lineage clone'"
+# 
+# 
+# for Q, qtext in DL_QueriesByID.iteritems():
+#     instances = vfb_ind.getInstances(Q, 0)
+#     if instances:
+#         print qtext + " - Individuals: " + str( len(instances) )
+#     else:
+#         warnings.warn(qtext + " has no instances!")
+#     instances = vfb_ind.subClasses(Q, 0)
+#     if subclasses:
+#         print qtext + " - subClasses: " + str( len(subclasses) )
+#     else:
+#         warnings.warn(qtext + " has no subclasses!")
+# 
+#         # Queries for individuals currently work on labels.  Unusually, the
+# 
+# # roll name_id lookup
+# 
+# name_id = {}
+# I = vfb_ind.getInstances("Thing", 0)
+# S = vfb_ind.SubClassOf("Thing", 0)
+# 
+# while i in I:
+#     name_id[vfb_ind.getLabel(i)] = i
+#     
+# while s in S:
+#     name_id[vfb_ind.getLabel(s)] = s
+# 
+# cluster = name_id['cluster']
+# neuron = name_id['neuron']
+# 
+#     # But how to get a list of object properties?
     
         
 vfb_ind.sleep()
