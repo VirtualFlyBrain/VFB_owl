80ab6fbffd133b402e155a9092d90504c3b3207c
David Osumi-Sutherland <dosumis@gmail.com>
2015-06-04 20:07:36 +0100

    Now works

--- src/code/db_maintenance/type_individuals_from_tsv.py
+++ src/code/db_maintenance/type_individuals_from_tsv.py
@@ -2,6 +2,7 @@
 from lmb_fc_tools import get_con, owlDbOnt
 from uk.ac.ebi.brain.core import Brain
 from tsv2pdm import tab
+import warnings
 import sys
 
 sys.path.append("../mod/")
@@ -20,7 +21,12 @@ annotation_table = tab(path = sys.argv[3], file_name=sys.argv[4])
 
 ID_range_start = 20000
 for row in annotation_table.tab:
-    new_ind = odbo.add_ind(name = row['ind_name'], source = row['ind_source'], ID_range_start = 20000)
+    print str(row)
+    new_ind = odbo.add_ind(name = row['ind_name'], source = row['ind_source'], ID_range_start = 20000) # Returns FALSE and warns if addn fails
+    if not new_ind:
+        new_ind = odbo.ind_NameId[row['ind_name']]
+        warnings.warn("Assuming existing individual called %s (%s) is the correct one, and adding types accordingly." % (new_ind, row['ind_name']))
+    print new_ind
     odbo.add_ind_type(ind = new_ind, OWLclass = row['class'], objectProperty = row['rel'])
 
 # Now make an OWL file for source to check new additions.  
