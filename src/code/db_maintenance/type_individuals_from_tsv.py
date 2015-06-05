ab84cc3757b753cdbb648af2fe88718a8d708542
David Osumi-Sutherland <dosumis@gmail.com>
2015-06-05 18:34:49 +0100

    Added more doc.  Script now working as advertised

--- src/code/db_maintenance/type_individuals_from_tsv.py
+++ src/code/db_maintenance/type_individuals_from_tsv.py
@@ -5,6 +5,22 @@ from tsv2pdm import tab
 import warnings
 import sys
 
+"""An simple script illustrating how to add annotations to the VFB_owl mySQL DB
+Requires Brain; lmb_fc_tools.py;  https://github.com/dosumis/tsv2pdm.
+1st ARG: username
+2nd ARG: pwd
+3rd ARG: path to table
+4th ARG: table name
+
+Notes:
+1. When looping through the table, this script assumes that any individual 
+matching a specified name is the correct one to add the type to.  If this happens
+warnings are triggered. It is straightforward to remove this assumption.
+
+2. Single quotes in names should NOT be escaped as the escape chars will end up in the DB.  
+Double quotes in names are banned.
+"""
+
 sys.path.append("../mod/")
 con = get_con(usr = sys.argv[1] , pwd = sys.argv[2])  # connection to LMB DB. Need to make ssh tunnel first.
 
@@ -12,12 +28,14 @@ ontologies = Brain() # Construct Brain object
 
 # Now load up ontologies.  These are used to check validity for addition of new classes or 
 # relations to DB. You can load as many as you need.
-ontologies.learn("http://purl.obolibrary.org/obo/fbbt/fbbt-simple.owl") # Better to use version URI
+ontologies.learn("http://purl.obolibrary.org/obo/fbbt/fbbt-simple.owl") # Switch to specific release if necessary.
 
 odbo = owlDbOnt(conn = con, ont = ontologies) # Object for interacting with the database + ontologies.
 # Also detects anything that looks like a FlyBase feature and checks validity against public FlyBase.
 
-annotation_table = tab(path = sys.argv[3], file_name=sys.argv[4])
+annotation_table = tab(path = sys.argv[3], file_name=sys.argv[4])  # tsv file with headers: ind_name, class, rel, ind_source
+
+# ind_source must already be in the DB
 
 ID_range_start = 20000
 for row in annotation_table.tab:
@@ -28,6 +46,8 @@ for row in annotation_table.tab:
         warnings.warn("Assuming existing individual called %s (%s) is the correct one, and adding types accordingly." % (new_ind, row['ind_name']))
     print new_ind
     odbo.add_ind_type(ind = new_ind, OWLclass = row['class'], objectProperty = row['rel'])
+    
+con.close()
 
 # Now make an OWL file for source to check new additions.  
 # Would be good to roll this into script, but needs to be packaged up a bit better
