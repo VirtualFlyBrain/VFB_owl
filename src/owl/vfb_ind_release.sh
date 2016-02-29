40270898bee356949c4d1b3f3fe700138e6981db
David Osumi-Sutherland <dosumis@gmail.com>
2016-02-29 11:37:52 +0000

    Updated to wrap each step in a check.  Whole script now fails with warning if singel step fails

--- src/owl/vfb_ind_release.sh
+++ src/owl/vfb_ind_release.sh
@@ -1,27 +1,45 @@
 #!/usr/bin/env bash
 
+exit_on_fail() { 
+    status=$1
+    message=$2
+    if [ "$status" != 0 ];
+    then echo "$message" " failed with exit status: " "$status:" ; exit 1;
+    else echo "$message successful";
+    return 0;
+    fi
+}
+
+progress_chat() {
+    echo ''
+    echo "*** $@ ***"
+}
+
 DATASETS=('Cachero2010' 'Ito2013' 'Jenett2012' 'Yu2013' 'JenettShinomya_BrainName')
 
 DSSTRING=''
 
-cd ../code/owl_gen
-echo ''
-echo 'Building ontology of FlyBase features'
 
-./fb_feat_ont_gen.py $USR $PD
-echo "Return stat: "$?
+cd ../code/owl_gen
+job='Building ontology of FlyBase features';
+progress_chat "$job";
+ java -Xmx6000m -cp $CP"*" org.python.util.jython fb_feat_ont_gen.py $USR $PD
+exit_on_fail $? "$job"
 
 for var in ${DATASETS[@]}
 # java -classpath lib/*.jar:. my.package.Program
 do
-    echo $var
+    job="Building $var inds";
+    progress_chat $job
     java -Xmx6000m -cp $CP"*" org.python.util.jython vfb_ind_runner.py $USR $PD $var $FBBT
     DSSTRING+="--merge ${var}.owl "
-    echo "Return stat: "$?
+    exit_on_fail $? "$job"
 done
 
+job="Adding analysis results to flycircuit neuron inds.";
+progress_chat $job
 java -Xmx6000m -cp $CP"*" org.python.util.jython fc_ind.py $USR $PD $FBBT
-echo "Return stat: "$?
+exit_on_fail $? $job
 
 DSSTRING+="--merge flycircuit_plus.owl --merge flycircuit_direct_mappings.owl "
 
@@ -29,31 +47,36 @@ cd ../../owl
 # # env should be moved to Jenkins job.  Should be actual list, with iteration deriving env with --merge in.
 
 # # Merge ind files with specified release of fbbt
-echo ''
-echo "*** Merging ind files with $RELEASE version of fbbt ***"
+job="Merging ind files with ${FBBT}" 
+progress_chat $job
 owltools $FBBT --merge fb_features.owl --merge vfb_ext.owl $DSSTRING -o file://`pwd`/fbbt_vfb_ind.owl
+exit_on_fail $? $job
 
-# # assert inferences and strip out redundancy  # This 
-echo ''
-echo "*** Asserting inferences and striping redundancy ***"
+# # assert inferences and strip out redundancy  # This
+job="Asserting inferences and striping redundancy" 
+progress_chat $job
 owltools fbbt_vfb_ind.owl --reasoner elk --reasoner-ask-all --remove-indirect -a INDIVIDUALS -o fbbt_vfb_ind_pr_nr.owl # Some special magic here. Best ask the owltools devs if you want to know how it works.
-echo ''
-echo '*** Setting URI ***'
+exit_on_fail $? $job
+
+$job="Setting URI"
+progress_chat $job
 # Ideally would save as functional syntax, but bug in owltools preventing this.
+$v = `date "+%Y-%m-%d"`
 owltools fbbt_vfb_ind_prc_nr.owl --set-ontology-id -v 'http://purl.obolibrary.org/obo/fbbt/vfb/'$v'vfb.owl' 'http://purl.obolibrary.org/obo/fbbt/vfb/vfb.owl' -o vfb.owl
+exit_on_fail $? $job
 
-
-echo ''
-echo '** Compressing **'
-
+$job="Compressing"
+progress_chat $job
 gzip -c vfb.owl > vfb.owl.gz
-
-# Running reporting tests on build
+exit_on_fail $? $job
 
 export p=`pwd`
 
 cd ../code/unit_tests
 
+$job="Running reporting tests on build"
+progress_chat $job
 ./query_test_mod.py $p'/vfb.owl' > $p'/test_results.txt'
+exit_on_fail $? $job
 
 
