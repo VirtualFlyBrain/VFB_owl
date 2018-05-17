9263eaf577467d129b610747c5015f90d3a1ae6a
David Osumi-Sutherland <dosumis@gmail.com>
2018-05-17 10:13:19 +0100

    Update vfb_ind_release.sh
    First attempt at a scala build

--- src/owl/vfb_ind_release.sh
+++ src/owl/vfb_ind_release.sh
@@ -47,15 +47,17 @@ for var in ${DATASETS[@]}
 do
     job="Building $var inds";
     progress_chat $job
-    java -Xmx6000m -Xss515m -cp $CP"*" org.python.util.jython vfb_ind_runner.py $KB_endpoint $USR $PD $var $FBBT
+#   java -Xmx6000m -Xss515m -cp $CP"*" org.python.util.jython vfb_ind_runner.py $KB_endpoint $USR $PD $var $FBBT
+    sbt \"run -f --usr=$USR --pwd=$PWD --endpoint=$ENDPOINT --dataset=$var  --ontology=$FBBT
     DSSTRING+="--merge ${var}.owl "
     exit_on_fail $? "$job"
 done
 
-job="Adding analysis flycircuit neuron inds, mappings and clusters.";
-progress_chat $job
-java -Xmx6000m -Xss515m -cp $CP"*" org.python.util.jython fc_ind.py $KB_endpoint $USR $PD $FBBT
-exit_on_fail $? $job
+#job="Adding analysis flycircuit neuron inds, mappings and clusters.";
+#progress_chat $job
+#java -Xmx6000m -Xss515m -cp $CP"*" org.python.util.jython fc_ind.py $KB_endpoint $USR $PD $FBBT
+#sbt \"run -f --usr=$USR --pwd=$PWD --endpoint=$ENDPOINT --dataset=""  --ontology=$FBBT
+#exit_on_fail $? $job
 
 DSSTRING+=" --merge flycircuit_plus.owl "  # Need to move these to KB.
 
@@ -101,4 +103,3 @@ job="Running reporting tests on build"
 progress_chat $job
 java -Xmx6000m -Xss515m -cp $CP"*" org.python.util.jython query_test_mod.py $p'/vfb.owl' > $p'/test_results.txt'
 exit_on_fail $? $job
-
